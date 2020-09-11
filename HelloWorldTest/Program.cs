160a6705a534c3db983b6f739271840a122555c9
VickTorr <70772578+VictoriaTorr@users.noreply.github.com>
2020-09-10 20:54:38 -0500

    Initial commit

---
+++ HelloWorldTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorldTest
+{
+    class Program
+    {
+        static void Main(string[] args);
+        {
+            Console.WriteLine("Hello World!");  
+            string aFriend = "Dani";
+            Console.WriteLine(aFriend);
+            aFriend = "Fatima";
+            Console.WriteLine(aFriend);
+        }
+    }
